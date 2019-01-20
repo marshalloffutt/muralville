baee95c6a3b682eeb063b36677a20daa04b9a3b2
Marshall Offutt <marshalloffutt@gmail.com>
2019-01-20 10:50:38 -0600

    Adds Auth component, login with google image appearing

--- src/App/App.js
+++ src/App/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'reactstrap';
+import connection from '../helpers/data/connection';
+import Auth from '../components/Auth/Auth';
 import './App.scss';
 
 class App extends React.Component {
+  componentDidMount() {
+    connection();
+  }
+
   render() {
     return (
-      <div className="App">
-        <button className='btn btn-danger'>Bootstrap test</button>
-        <Button
-            tag="a"
-            color="success"
-            size="large"
-            href="http://reactstrap.github.io"
-            target="_blank"
-        >
-            View Reactstrap Docs
-        </Button>
+      <div className="Auth">
+        <Auth />
       </div>
     );
   }
