593f978cabf1a0192358cc897f33a2ffc7666cd7
Marshall Offutt <marshalloffutt@gmail.com>
2019-01-20 11:29:20 -0600

    Adds styyyyle to login button; completes login portion of auth

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,47 @@
 import React from 'react';
+import firebase from 'firebase/app';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import connection from '../helpers/data/connection';
 import Auth from '../components/Auth/Auth';
 import './App.scss';
 
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
   componentDidMount() {
     connection();
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          authed: true,
+        });
+      } else {
+        this.setState({
+          authed: false,
+        });
+      }
+    });
+  }
+
+  isAuthenticated = () => {
+    this.removeListener();
   }
 
   render() {
+    const { authed } = this.state;
+
+    if (!authed) {
+      return (
+        <div className="App">
+          <Auth />
+        </div>
+      );
+    }
     return (
-      <div className="Auth">
-        <Auth />
+      <div className="App">
+        <h1>You're in!!!</h1>
       </div>
     );
   }
--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Button } from 'reactstrap';
+import authRequests from '../../helpers/data/authRequests';
 import './Auth.scss';
 
 class Auth extends React.Component {
+  static propTypes = {
+    isAuthenticated: PropTypes.func,
+  }
+
+  authenticateUser = (e) => {
+    e.preventDefault();
+    authRequests.authenticate().then(() => {
+      this.props.isAuthenticated();
+    }).catch(err => console.error('there was an error'));
+  }
+
   render() {
     return (
-      <input className="login-btn" type="image" src="https://www.htps.us/UserFiles/Servers/Server_791028/Templates/login-google.png" alt="login with google"/>
+      <Button outline color="danger" className="loginBtn" onClick={this.authenticateUser}>
+        <i className="fab fa-google-plus-square fa-2x btnIcon"></i>
+        <span className="btnText"> Sign in with Google</span>
+      </Button>
     );
   }
 }
