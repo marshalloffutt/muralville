60ba6194988ce2b7e51c93648360d7cf099f00ea
Marshall Offutt <marshalloffutt@gmail.com>
2019-01-23 18:11:57 -0600

    onChange now changes newMural.title in state woohoo!

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,8 +2,8 @@ import React from 'react';
 import {
   Button, Modal, ModalHeader, ModalBody, ModalFooter,
 } from 'reactstrap';
-import PropTypes from 'prop-types';
-import authRequests from '../../helpers/data/authRequests';
+// import PropTypes from 'prop-types';
+// import authRequests from '../../helpers/data/authRequests';
 
 const defaultMural = {
   title: '',
@@ -13,18 +13,22 @@ const defaultMural = {
   uid: '',
 };
 
-state = {
-  newMural: defaultMural,
-}
-
 class ModalExample extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       modal: false,
+      newMural: defaultMural,
     };
 
     this.toggle = this.toggle.bind(this);
+  }
+
+  formFieldStringState = (name, e) => {
+    e.preventDefault();
+    const tempMural = { ...this.state.newMural };
+    tempMural[name] = e.target.value;
+    this.setState({ newMural: tempMural });
   }
 
   toggle() {
@@ -33,14 +37,31 @@ class ModalExample extends React.Component {
     });
   }
 
+  titleChange = e => this.formFieldStringState('title', e);
+
   render() {
+    const { newMural } = this.state;
     return (
       <div>
         <Button color="danger" onClick={this.toggle}>{this.props.buttonLabel}</Button>
         <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
           <ModalHeader toggle={this.toggle}>New Mural</ModalHeader>
           <ModalBody>
-            words words words
+            <form>
+              <div className="form-group">
+                <label htmlFor="title">Title:</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="title"
+                  aria-describedby="titleHelp"
+                  placeholder="Mural Title"
+                  value={newMural.title}
+                  onChange={this.titleChange}
+                />
+              </div>
+              <button className="btn btn-danger">Save Listing</button>
+            </form>
           </ModalBody>
           <ModalFooter>
             <Button color="primary" onClick={this.toggle}>Save Mural</Button>{' '}
