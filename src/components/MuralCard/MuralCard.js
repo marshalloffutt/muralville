33c8748d2cf840210fd78738da7e6d48d4bb376f
Marshall Offutt <marshalloffutt@gmail.com>
2019-02-02 11:03:54 -0600

    Adds star symbol to cards, and a click event

--- src/components/MuralCard/MuralCard.js
+++ src/components/MuralCard/MuralCard.js
@@ -17,17 +17,22 @@ class MuralCard extends React.Component {
     initializeSingleCardView(muralId, selectedMural);
   }
 
+  favoriteToggle = (e) => {
+    e.preventDefault();
+    console.log('you clicked a star!!!!!');
+  }
+
   render() {
     const { mural } = this.props;
     return (
-      <div className="m-4 mural-item" id={mural.id} onClick={this.clickedCard}>
-        <div className="card mural-card">
-          <div className="pt-3 pl-3 pr-3">
-            <div className="container card-img-container">
-              <img className="card-img-top" src={mural.image} alt={mural.title}/>
-              <button className="btn card-img-button"><i className="fas fa-star fa-2x"></i></button>
+      <div className="parent-div p-0 m-0">
+        <i className="fas fa-star fa-2x card-img-button" onClick={this.favoriteToggle}></i>
+        <div className="m-4 mural-item" id={mural.id} onClick={this.clickedCard}>
+          <div className="card mural-card">
+            <div className="pt-3 pl-3 pr-3">
+                <img className="card-img-top" src={mural.image} alt={mural.title}/>
+                <h3 className="card-title mural-card-title mt-1">{mural.title}</h3>
             </div>
-              <h3 className="card-title mural-card-title mt-1">{mural.title}</h3>
           </div>
         </div>
       </div>
